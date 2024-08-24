30f89b147c05af24c2eaba2742005bf3ca8fd8d6
Keren Samama <ksamama@g.jct.ac.il>
2024-08-24 22:11:14 +0300

    delete

--- vite-project/src/Router.jsx
+++ vite-project/src/Router.jsx
@@ -5,27 +5,22 @@ import MessagePage from "./pages/MessagePage.jsx";
 import Login from "./components/Login.jsx";
 import Board from "./pages/Board.jsx"
 import SignUp from './pages/SignUp.jsx';
+import UserPage from "./pages/UserPage.jsx";
+import User from "./components/User.jsx";
 
-
-
-
- export default function Router() {
-
+export default function Router() {
    return (
-    
      <BrowserRouter>
        <Routes>
          <Route path="/login" element={<Login/>} />
+         <Route path="/users" element ={<UserPage/>}/>
+         <Route path="/users/:id" element ={<User />}/>
          <Route path = "/signUp" element = {<SignUp/>}/>
          <Route path="/board/:id" element={<Board />} />
          <Route path="/posts" element={<PostPage />} />
          <Route path="/messages" element={<MessagePage />} />
          <Route path="/messages/:id" element={<Message/>} />
-
-
        </Routes>
      </BrowserRouter>
    );
- }
-
- 
+}
